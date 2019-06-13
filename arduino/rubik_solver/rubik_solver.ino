c881896a4172c48f8ec5f72c7842359e65c2ae9c
Marco Antonio Paiva <marcopaivaa@gmail.com>
2019-06-13 13:50:18 -0300

    Ajustes

--- arduino/rubik_solver/rubik_solver.ino
+++ arduino/rubik_solver/rubik_solver.ino
@@ -47,11 +47,15 @@ void init_servos();
 #define ARM_MAX_DEGREE          0
 #define ARM_MIN_DEGREE          180
 
-#define TRAIL_MAX_DEGREE        108
+#define TRAIL_DEFAULT_DEGREE    110
+#define TRAIL_MAX_DEGREE        112
 #define TRAIL_MIN_DEGREE        0
 
+
 #define DEBUG                   1
-#define DEBUG_COMMAND           0 //index of command array
+#define DEBUG_COMMAND           5 //index of command array
+
+#define DELAY                   1000
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 String commands[] = {"L", "R", "F", "B", "T", "D", "L'", "R'", "F'", "B'", "T'", "D'"};
@@ -83,51 +87,51 @@ void loop() {
 /** FUNCTIONS */
 
 void left(int direction){
-    int degree = direction > 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
+    int degree = direction < 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
     rotate(degree, LEFT_ARM_PIN, LEFT_ARM_MIN, LEFT_ARM_MAX, 1);
     rotate(TRAIL_MIN_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 1);
     rotate(ARM_DEFAULT_DEGREE, LEFT_ARM_PIN, LEFT_ARM_MIN, LEFT_ARM_MAX, 1);
-    rotate(TRAIL_MAX_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 1);
+    rotate(TRAIL_DEFAULT_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 1);
 }
 
 
 void right(int direction){
-    int degree = direction > 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
+    int degree = direction < 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
     rotate(degree, RIGHT_ARM_PIN, RIGHT_ARM_MIN, RIGHT_ARM_MAX, 1);
     rotate(TRAIL_MIN_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 1);
     rotate(ARM_DEFAULT_DEGREE, RIGHT_ARM_PIN, RIGHT_ARM_MIN, RIGHT_ARM_MAX, 1);
-    rotate(TRAIL_MAX_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 1);
+    rotate(TRAIL_DEFAULT_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 1);
 }
 
 
 void front(int direction){
-    int degree = direction > 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
+    int degree = direction < 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
     rotate(degree, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 1);
     rotate(TRAIL_MIN_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 1);
     rotate(ARM_DEFAULT_DEGREE, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 1);
-    rotate(TRAIL_MAX_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 1);
+    rotate(TRAIL_DEFAULT_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 1);
 }
 
 
 void back(int direction){
-    int degree = direction > 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
+    int degree = direction < 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
     rotate(degree, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 1);
     rotate(TRAIL_MIN_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 1);
     rotate(ARM_DEFAULT_DEGREE, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 1);
-    rotate(TRAIL_MAX_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 1);
+    rotate(TRAIL_DEFAULT_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 1);
 }
 
 
 void top(int direction){
     full_rotate(1);
-    right(direction);
+    left(direction);
     full_rotate(-1);
 }
 
 
 void down(int direction){
     full_rotate(1);
-    left(direction);
+    right(direction);
     full_rotate(-1);
 }
 
@@ -136,7 +140,7 @@ void rotate(int deg, int pin, int min, int max, int delay_bit){
     uint16_t pulse = map(deg, 0, 180, min, max);
     pwm.setPWM(pin, 0, pulse);
     if(delay_bit == 1){
-        delay(1000);
+        delay(DELAY);
     }
 }
 
@@ -144,8 +148,10 @@ void full_rotate(int direction){
 
     rotate(TRAIL_MIN_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 0);
     rotate(TRAIL_MIN_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 0);
+    delay(DELAY);
 
-
+    rotate(TRAIL_MAX_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
+    rotate(TRAIL_MAX_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
     if(direction > 0){ //top-down position
         
         rotate(ARM_MAX_DEGREE, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 0);
@@ -157,18 +163,25 @@ void full_rotate(int direction){
         rotate(ARM_MAX_DEGREE, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 0);
 
     }
+    delay(DELAY);
 
-    rotate(TRAIL_MAX_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 0);
-    rotate(TRAIL_MAX_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 0);
+    delay(DELAY);
 
     rotate(TRAIL_MIN_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
     rotate(TRAIL_MIN_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    delay(DELAY);
 
     rotate(ARM_DEFAULT_DEGREE, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 0);
     rotate(ARM_DEFAULT_DEGREE, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 0);
+    delay(DELAY);
 
-    rotate(TRAIL_MAX_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
-    rotate(TRAIL_MAX_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    delay(DELAY);
 }
 
 void init_servos(){
@@ -177,19 +190,21 @@ void init_servos(){
     rotate(TRAIL_MIN_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
     rotate(TRAIL_MIN_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
 
-    delay(2500);
+    delay(DELAY);
 
     rotate(ARM_DEFAULT_DEGREE, LEFT_ARM_PIN, LEFT_ARM_MIN, LEFT_ARM_MAX, 0);
     rotate(ARM_DEFAULT_DEGREE, RIGHT_ARM_PIN, RIGHT_ARM_MIN, RIGHT_ARM_MAX, 0);
     rotate(ARM_DEFAULT_DEGREE, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 0);
     rotate(ARM_DEFAULT_DEGREE, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 0);
 
-    delay(2500);
+    delay(DELAY);
 
-    rotate(TRAIL_MAX_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 0);
-    rotate(TRAIL_MAX_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 0);
-    rotate(TRAIL_MAX_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
-    rotate(TRAIL_MAX_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, LEFT_TRAIL_PIN, LEFT_TRAIL_MIN, LEFT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, RIGHT_TRAIL_PIN, RIGHT_TRAIL_MIN, RIGHT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
+    rotate(TRAIL_DEFAULT_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
+    
+    delay(DELAY);
 }
 
 void exec_command(int com){
