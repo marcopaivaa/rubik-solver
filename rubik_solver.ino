36d346d99588d3e5b18fdd0314fbf340fd1f1fb6
jownn <jonathansouzasilva@hotmail.com>
2019-09-28 20:27:39 -0300

    Ajustes para utilizar serial

--- rubik_solver.ino
+++ rubik_solver.ino
@@ -8,7 +8,7 @@ void left(int direction);
 void right(int direction);
 void front(int direction);
 void back(int direction);
-void top(int direction);
+void upper(int direction);
 void down(int direction);
 void rotate(int deg, int pin, int min, int max);
 void full_rotate(int direction);
@@ -47,18 +47,19 @@ void init_servos();
 #define ARM_MAX_DEGREE          0
 #define ARM_MIN_DEGREE          180
 
-#define TRAIL_DEFAULT_DEGREE    110
-#define TRAIL_MAX_DEGREE        112
+#define TRAIL_DEFAULT_DEGREE    100
+#define TRAIL_MAX_DEGREE        100
 #define TRAIL_MIN_DEGREE        0
 
 
-#define DEBUG                   1
-#define DEBUG_COMMAND           5 //index of command array
+#define DEBUG                   0
+#define DEBUG_COMMAND           0 //index of command array
 
 #define DELAY                   1000
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-String commands[] = {"L", "R", "F", "B", "T", "D", "L'", "R'", "F'", "B'", "T'", "D'"};
+String commands[] = {"L", "R", "F", "B", "U", "D", "L'", "R'", "F'", "B'", "U'", "D'"};
+int numCommands = 12;
 int debug_executed = 0;
 
 void setup() {
@@ -77,14 +78,60 @@ void loop() {
         debug_executed = 1;
         exit(0);
     }else{
-        int r = rand() % 12;
-        //Serial.println("[*] Running: %s", commands[r]);
-        exec_command(r);
+      if (Serial.available() > 0) {
+        String recebido = leStringSerial();
+        Serial.println(recebido);
+        char buf[100];
+        recebido.toCharArray(buf, 100);
+        char delim[] = " ";
+      
+        char *ptr = strtok(buf, delim);
+      
+        while(ptr != NULL)
+        {
+          for(int i = 0; i < numCommands; i++){
+            if(String(ptr) == commands[i]){
+              Serial.println(ptr);
+              exec_command(i);
+            }
+          } 
+          ptr = strtok(NULL, delim);
+        }
+        
+        
+//        int r = rand() % numCommands;
+//        //Serial.println("[*] Running: %s", commands[r]);
+//        exec_command(r);
+      }   
     }
 }
 
 
 /** FUNCTIONS */
+
+/**
+ * Função que lê uma string da Serial
+ * e retorna-a
+ */
+String leStringSerial(){
+  String conteudo = "";
+  char caractere;
+  
+  // Enquanto receber algo pela serial
+  while(Serial.available() > 0) {
+    // Lê byte da serial
+    caractere = Serial.read();
+    // Ignora caractere de quebra de linha
+    if (caractere != '\n'){
+      // Concatena valores
+      conteudo.concat(caractere);
+    }
+    // Aguarda buffer serial ler próximo caractere
+    delay(10);
+  }
+    
+  return conteudo;
+}
 
 void left(int direction){
     int degree = direction < 0 ? ARM_MAX_DEGREE : ARM_MIN_DEGREE;
@@ -122,7 +169,7 @@ void back(int direction){
 }
 
 
-void top(int direction){
+void upper(int direction){
     full_rotate(1);
     left(direction);
     full_rotate(-1);
@@ -152,7 +199,7 @@ void full_rotate(int direction){
 
     rotate(TRAIL_MAX_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
     rotate(TRAIL_MAX_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
-    if(direction > 0){ //top-down position
+    if(direction > 0){ //upper-down position
         
         rotate(ARM_MAX_DEGREE, FRONT_ARM_PIN, FRONT_ARM_MIN, FRONT_ARM_MAX, 0);
         rotate(ARM_MIN_DEGREE, BACK_ARM_PIN, BACK_ARM_MIN, BACK_ARM_MAX, 0);
@@ -190,7 +237,7 @@ void init_servos(){
     rotate(TRAIL_MIN_DEGREE, FRONT_TRAIL_PIN, FRONT_TRAIL_MIN, FRONT_TRAIL_MAX, 0);
     rotate(TRAIL_MIN_DEGREE, BACK_TRAIL_PIN, BACK_TRAIL_MIN, BACK_TRAIL_MAX, 0);
 
-    delay(DELAY);
+    delay(5000);
 
     rotate(ARM_DEFAULT_DEGREE, LEFT_ARM_PIN, LEFT_ARM_MIN, LEFT_ARM_MAX, 0);
     rotate(ARM_DEFAULT_DEGREE, RIGHT_ARM_PIN, RIGHT_ARM_MIN, RIGHT_ARM_MAX, 0);
@@ -218,7 +265,7 @@ void exec_command(int com){
         case 3:
             back(1); break;
         case 4:
-            top(1); break;
+            upper(1); break;
         case 5:
             down(1); break;
         case 6:
@@ -230,7 +277,7 @@ void exec_command(int com){
         case 9:
             back(-1); break;
         case 10:
-            top(-1); break;
+            upper(-1); break;
         case 11:
             down(-1); break;
     }
